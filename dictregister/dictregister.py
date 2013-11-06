5241ef7dfaca789b2e406daa8038dd6c295a89b3
Leonardo Giordani <giordani.leonardo@gmail.com>
2013-11-06 18:48:05 +0100

    First code and tests

--- dictregister/dictregister.py
+++ dictregister/dictregister.py
@@ -1,2 +1,145 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import collections
+
+class DictRegister(list):
+    def __init__(self, iterable=[]):
+        """This class accepts an iterable of dictionary-like objects.
+        If an element is not a dictionary-like object raises ValueError.
+        """
+        for elem in iterable:
+            self._check_elem(elem)
+        super(DictRegister, self).__init__(iterable)
+
+    def _check_elem(self, elem):
+        # Check if the given element is a dictionary-like object
+        try:
+            elem.iteritems
+        except AttributeError:
+            raise ValueError, "Given element %s is not a dictionary-like object" %(elem)
+
+    def append(self, elem):
+        self._check_elem(elem)
+        super(DictRegister, self).append(elem)
+
+    def kadd(self, key, value):
+        """Adds the given key/value to all elements.
+        Single values for a key are stored directly, as key:value, multiple
+        values for the same key are stored as key,set(values).
+        """
+        for item in self:
+            try:
+                # Use the key as a set
+                item[key].add(value)
+            except KeyError:
+                # This happens if the key is not present
+                item[key] = value
+            except AttributeError:
+                # This happens if the key is present but is not a set
+                item[key] = set([item[key], value])
+
+    def kreplace(self, key, value):
+        """Replaces the given key/value for each element.
+        If the key is not present silently passes.
+        """
+        for item in self:
+            if key in item:
+                item[key] = value
+
+    def kremove(self, key, value=None):
+        """Removes the given key/value from all elements.
+        If value is not specified, the whole key is removed.
+        If value is not None and the key is present but with a
+        different value, or if the key is not present, silently passes.
+        """
+        for item in self:
+            if value is None:
+                # Just pop the key if present,
+                # otherwise return None
+                # (shortcut to ignore the exception)
+                item.pop(key, None)
+            else:
+                try:
+                    # Use the key as a set
+                    item[key].remove(value)
+                    # If the set contains a single element
+                    # just store the latter
+                    if len(item[key]) == 1:
+                        item[key] = item[key].pop()
+                except KeyError:
+                    # This happens when the item
+                    # does not contain the key
+                    pass
+                except AttributeError:
+                    # This happens when the key is not a set
+                    # and shall be removed only if values match
+                    if item[key] == value:
+                        item.pop(key)
+
+
+    def _match(self, item, keyop, value):
+        # Split key and operator
+        if '__' not in keyop:
+            keyop = keyop + '__eq'
+        key, op = keyop.split('__')
+
+        if op == "eq":
+            try:
+                return item[key] == value
+            except KeyError:
+                return False
+        elif op == "ne":
+            try:
+                return item[key] != value
+            except KeyError:
+                return True
+        elif op == "in":
+            try:
+                if isinstance(item[key], collections.Iterable):
+                    return value in item[key]
+                else:
+                    return item[key] == value
+            except KeyError:
+                return False
+        elif op == "nin":
+            try:
+                if isinstance(item[key], collections.Iterable):
+                    return value not in item[key]
+                else:
+                    return item[key] != value
+            except KeyError:
+                return True
+        elif op == "iskey":
+            return (key in item) == value
+        else:
+            return False
+
+    def dfilter(self, **kwds):
+        starting_list = self._list
+        filtered_list = []
+        for key, value in kwds.iteritems():
+            for item in starting_list:
+                if self._match(item, key, value):
+                    filtered_list.append(item)
+            starting_list = filtered_list
+            filtered_list = []
+        
+        return DictRegister(starting_list)
+
+    def dget(self, **kwds):
+        return self.dfilter(**kwds)[0]
+        
+    def dpop(self, **kwds):
+        filtered_dr = self.dfilter(**kwds)
+        item = filtered_dr[0]
+        self.remove(item)
+        return item
+
+    def dremove(self, **kwds):
+        filtered_dr = self.dfilter(**kwds)
+        for item in self._list:
+            if item in filtered_dr:
+                self.remove(item)
+        return filtered_dr
+
--- tests/test_dictregister.py
+++ tests/test_dictregister.py
@@ -8,21 +8,75 @@ test_dictregister
 Tests for `dictregister` module.
 """
 
-import unittest
+import pytest
 
-from dictregister import dictregister
+from dictregister import dictregister as dr
+
+@pytest.fixture
+def fixdr():
+    return dr.DictRegister([{'x':1,'y':2},{'x':3,'y':4}])
+
+@pytest.fixture
+def fixdrmult():
+    return dr.DictRegister([{'x':set([1,3]),'y':2},{'x':3,'y':4}])
+
+def test_instance_without_parameters():
+    d = dr.DictRegister()
+
+def test_instance_with_a_list_of_dicts(fixdr):
+    assert len(fixdr) == 2
+
+def test_can_be_indexed(fixdr):
+    assert fixdr[0] == {'x':1,'y':2}
+
+def test_instance_with_an_argument():
+    with pytest.raises(ValueError):
+        d = dr.DictRegister([1,2,3])
+
+def test_append_dict(fixdr):
+    fixdr.append({'x':4,'y':5})
+    assert len(fixdr) == 3
+
+def test_cannot_append_non_dict(fixdr):
+    with pytest.raises(ValueError):
+        fixdr.append(6)
+
+def test_add_keyword(fixdr):
+    fixdr.kadd('z', 3)
+    assert len(fixdr) == 2
+    assert fixdr[0] == {'x':1,'y':2,'z':3}
+
+def test_add_already_present_keyword(fixdr):
+    fixdr.kadd('x', 3)
+    assert len(fixdr) == 2
+    assert fixdr[0] == {'x': set([1,3]),'y':2}
+    
+def test_replace_already_present_keyword(fixdr):
+    fixdr.kreplace('x', 3)
+    assert len(fixdr) == 2
+    assert fixdr[0] == {'x': 3,'y':2}
+
+def test_replace_not_present_keyword(fixdr):
+    fixdr.kreplace('z', 3)
+    assert len(fixdr) == 2
+    assert fixdr[0] == {'x': 1,'y':2}
+
+def test_remove_keyword(fixdr):
+    fixdr.kremove('x')
+    assert len(fixdr) == 2
+    assert fixdr[0] == {'y':2}
+    assert fixdr[1] == {'y':4}
+
+def test_remove_keyword_with_multiple_values(fixdrmult):
+    fixdrmult.kremove('x',3)
+    assert len(fixdrmult) == 2
+    assert fixdrmult[0] == {'x':1,'y':2}
+    assert fixdrmult[1] == {'y':4}
+
+def test_remove_not_present_keyword(fixdr):
+    fixdr.kremove('z')
+    assert len(fixdr) == 2
+    assert fixdr[0] == {'x':1,'y':2}
+    assert fixdr[1] == {'x':3,'y':4}
 
 
-class TestDictregister(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def test_something(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
